eb26f69f895a88a372baf4fd977c25558a9eb2d4
agj <ale@agj.cl>
2021-06-13 19:10:55 -0400

    Start adding transpilation code

---
+++ source/build.ts
@@ -0,0 +1,52 @@
+import glob from "glob-promise";
+import fs from "fs/promises";
+import babel from "@babel/core";
+import { replace } from "ramda";
+
+const pathRegex = /utilities\/([^/]+)\/([^/]+)\//;
+const getGroup = (path: string) => path.match(pathRegex)?.[1] ?? "";
+const getName = (path: string) => path.match(pathRegex)?.[2] ?? "";
+const postprocessTs = (name: string, code: string) =>
+  code.replace("export default ", `const ${name} = `);
+const postprocessJs = (code: string) => code.replace(/"use strict";(\n)+/, "");
+
+const babelConfigModern = {
+  filename: "index.ts",
+  presets: ["@babel/preset-typescript", "@babel/preset-env"],
+  targets: "defaults",
+} as any;
+const babelConfigLegacy = {
+  filename: "index.ts",
+  presets: ["@babel/preset-typescript", "@babel/preset-env"],
+} as any;
+
+type Utility = {
+  group: string;
+  name: string;
+  tsCode: string;
+};
+
+const utilityFiles = await glob(`./utilities/*/*/index.ts`);
+
+const getUtilities = () =>
+  Promise.all(
+    utilityFiles.map(async (path) => {
+      const name = getName(path);
+      const tsCode = postprocessTs(name, await fs.readFile(path, "utf-8"));
+      const jsCodeModern = postprocessJs(
+        babel.transformSync(tsCode, babelConfigModern).code
+      );
+      const jsCodeLegacy = postprocessJs(
+        babel.transformSync(tsCode, babelConfigLegacy).code
+      );
+      return {
+        group: getGroup(path),
+        name,
+        tsCode,
+        jsCodeModern,
+        jsCodeLegacy,
+      };
+    })
+  );
+
+getUtilities().then(console.log);
