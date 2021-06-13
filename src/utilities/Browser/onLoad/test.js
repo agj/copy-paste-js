2f12dd159c5c9387cd02cbbe15260215d748ca4e
agj <ale@agj.cl>
2021-06-12 20:55:54 -0400

    Some changes not committed

--- src/utilities/Browser/onLoad/test.js
+++ src/utilities/Browser/onLoad/test.js
@@ -1,15 +1,13 @@
 'use strict';
 
-module.exports = function (onLoad, window) {
-	return function (assert) {
-		assert.plan(3);
+module.exports = (onLoad, window) => assert => {
+	assert.plan(3);
 
-		var first = true;
-		onLoad(function () {
-			assert.false(first);
-			assert.equal(window.document.readyState, 'complete');
-		});
-		assert.true(first);
-		first = false;
-	};
-};
+	let first = true;
+	onLoad(() => {
+		assert.false(first);
+		assert.equal(window.document.readyState, 'complete');
+	});
+	assert.true(first);
+	first = false;
+}
