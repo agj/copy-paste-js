f9e5ab2af03786496a5b9bbbd3b83c2ad6a06a05
agj <ale@agj.cl>
2021-06-26 09:50:19 -0400

    Add Function/debounce compatible version

--- source/utilities/Function/debounce/spec.ts
+++ source/utilities/Function/debounce/spec.ts
@@ -1,20 +1,24 @@
 import debounce from "./";
+import debounceEs5 from "./compatible";
+
+const testWith = (debounce) => async () => {
+  const spy = jest.fn();
+
+  const f = debounce(0.01, spy);
+
+  f("first");
+  f("second");
+  f("third");
+
+  expect(spy).toHaveBeenCalledTimes(0);
+
+  jest.advanceTimersByTime(1000);
+
+  expect(spy).toHaveBeenCalledTimes(1);
+  expect(spy).toHaveBeenLastCalledWith("third");
+};
 
 describe("debounce", () => {
-  test("test", async () => {
-    const spy = jest.fn();
-
-    const f = debounce(0.01, spy);
-
-    f("first");
-    f("second");
-    f("third");
-
-    expect(spy).toHaveBeenCalledTimes(0);
-
-    jest.advanceTimersByTime(1000);
-
-    expect(spy).toHaveBeenCalledTimes(1);
-    expect(spy).toHaveBeenLastCalledWith("third");
-  });
+  test("typescript", testWith(debounce));
+  test("compatible", testWith(debounceEs5));
 });
