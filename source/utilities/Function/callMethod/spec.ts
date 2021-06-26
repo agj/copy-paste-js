40128bc087d89b88b61d182d6882c6969ffc1159
agj <ale@agj.cl>
2021-06-26 09:48:40 -0400

    Improve Function/callMethod tests

--- source/utilities/Function/callMethod/spec.ts
+++ source/utilities/Function/callMethod/spec.ts
@@ -1,19 +1,25 @@
 import callMethod from "./";
 import callMethodEs5 from "./compatible.js";
 
+const testWith = (callMethod) => async () => {
+  const spyThis = jest.fn().mockReturnThis();
+  const spy2 = jest.fn();
+  const object = {
+    methodThis: spyThis,
+    method2: spy2,
+  };
+
+  const return1 = callMethod("methodThis")(object);
+
+  expect(spyThis).toHaveBeenLastCalledWith();
+  expect(return1).toBe(object);
+
+  callMethod("method2", ["value", 2])(object);
+
+  expect(spy2).toHaveBeenLastCalledWith("value", 2);
+};
+
 describe("callMethod", () => {
-  test("test", async () => {
-    var f = function () {
-      return this;
-    };
-    var o = { m: f };
-    expect(callMethod("m")(o)).toEqual(o);
-    expect(callMethodEs5("m")(o)).toEqual(o);
-    var g = function (a) {
-      return a;
-    };
-    var p = { m: g };
-    expect(callMethod("m", ["a"])(p)).toBe("a");
-    expect(callMethodEs5("m", ["a"])(p)).toBe("a");
-  });
+  test("typescript", testWith(callMethod));
+  test("compatible", testWith(callMethodEs5));
 });
