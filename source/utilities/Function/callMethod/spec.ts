59899a9d58898c6f3cf17e4fb6401793888f9b26
agj <ale@agj.cl>
2021-06-26 11:07:54 -0400

    Begin improving tests with property-based testing

--- source/utilities/Function/callMethod/spec.ts
+++ source/utilities/Function/callMethod/spec.ts
@@ -1,21 +1,60 @@
 import callMethod from "./";
 
+import { testProp, fc } from "jest-fast-check";
+
 describe("callMethod", () => {
-  test("test", async () => {
-    const spyThis = jest.fn().mockReturnThis();
-    const spy2 = jest.fn();
+  testProp(
+    "creates a function that calls the appropriate method",
+    [fc.string()],
+    async (methodName) => {
+      const spy = jest.fn();
+      const object = {
+        [methodName]: spy,
+      };
+
+      expect(spy).toHaveBeenCalledTimes(0);
+
+      callMethod(methodName)(object);
+      callMethod(methodName)(object);
+
+      expect(spy).toHaveBeenCalledTimes(2);
+    }
+  );
+
+  test("creates a function that preserves `this`", async () => {
+    const spy = jest.fn().mockReturnThis();
     const object = {
-      methodThis: spyThis,
-      method2: spy2,
+      methodName: spy,
     };
 
-    const return1 = callMethod("methodThis")(object);
+    const return1 = callMethod("methodName")(object);
 
-    expect(spyThis).toHaveBeenLastCalledWith();
+    expect(spy).toHaveBeenLastCalledWith();
     expect(return1).toBe(object);
-
-    callMethod("method2", ["value", 2])(object);
-
-    expect(spy2).toHaveBeenLastCalledWith("value", 2);
   });
+
+  testProp(
+    "creates a function that passes all of its received arguments to the method",
+    [
+      fc.array(
+        fc.oneof(
+          fc.integer(),
+          fc.float(),
+          fc.string(),
+          fc.boolean(),
+          fc.dictionary(fc.string(), fc.float())
+        )
+      ),
+    ],
+    async (args) => {
+      const spy = jest.fn();
+      const object = {
+        method: spy,
+      };
+
+      callMethod("method", args)(object);
+
+      expect(spy).toHaveBeenLastCalledWith(...args);
+    }
+  );
 });
